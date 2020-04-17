b2a551bb6bad3eb0dc0a6d52fa07912aa0694792
lopez damian <dennyslopez2013@gmail.com>
2020-04-17 13:18:40 -0500

    change title

--- skate_shop_app/lib/app.dart
+++ skate_shop_app/lib/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Skate Shop",
+      title: "Retro Skate",
       theme: ThemeData(primarySwatch:Colors.deepPurple),
       home: Home(),
     );
