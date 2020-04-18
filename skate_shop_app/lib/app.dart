8b0c880e5efb1eea033721977de41d4d9eaa3193
lopez damian <dennyslopez2013@gmail.com>
2020-04-17 20:48:23 -0500

    change primary and add AppColors

--- skate_shop_app/lib/app.dart
+++ skate_shop_app/lib/app.dart
@@ -4,11 +4,26 @@ import 'package:flutter/material.dart';
 import 'package:skate_shop_app/screens/home/home.dart';
 
 class App extends StatelessWidget {
+  static Map<int, Color> _color = {
+    50: Color.fromRGBO(48, 25, 52, .1),
+    100: Color.fromRGBO(48, 25, 52, .2),
+    200: Color.fromRGBO(48, 25, 52, .3),
+    300: Color.fromRGBO(48, 25, 52, .4),
+    400: Color.fromRGBO(48, 25, 52, .5),
+    500: Color.fromRGBO(48, 25, 52, .6),
+    600: Color.fromRGBO(48, 25, 52, .7),
+    700: Color.fromRGBO(48, 25, 52, .8),
+    800: Color.fromRGBO(48, 25, 52, .9),
+    900: Color.fromRGBO(48, 25, 52, 1),
+    900: Color.fromRGBO(48, 25, 52, 1),
+  };
+  static MaterialColor _colorCustom = MaterialColor(0xFF301934, _color);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Retro Skate",
-      theme: ThemeData(primarySwatch:Colors.deepPurple),
+      theme: ThemeData(primarySwatch: _colorCustom),
       home: Home(),
     );
   }
---
+++ skate_shop_app/lib/values/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  //  #301934 ------ 48, 25, 52
+  static Color _primaryColor = Color.fromRGBO(48, 25, 52, 1);
+
+  static Color get primaryColor => _primaryColor;
+
+
+}
